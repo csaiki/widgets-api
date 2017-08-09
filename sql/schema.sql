a2b517ce324521cb026ba6cc658458cc85ec3503
christiansaiki <christian@revmob.com>
2017-08-08 21:18:11 -0300

    generated sql files

--- sql/schema.sql
+++ sql/schema.sql
@@ -5,24 +5,26 @@ USE widgetsapi;
 DROP TABLE IF EXISTS users;
 CREATE TABLE users
 (
-  id              INT unsigned NOT NULL, # Unique ID for the record
+  id              INT unsigned NOT NULL AUTO_INCREMENT, # Unique ID for the record
   name            VARCHAR(150) NOT NULL,                # Name of the user
   gravatar        VARCHAR(150) NOT NULL,
-  created_at timestamp DEFAULT NULL,
-  updated_at timestamp DEFAULT NULL,
-  deleted_at timestamp DEFAULT NULL,
+  created_at timestamp NULL DEFAULT NULL,
+  updated_at timestamp NULL DEFAULT NULL,
+  deleted_at timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS widgets;
 CREATE TABLE widgets
 (
-	id INT unsigned NOT NULL,
+	id INT unsigned NOT NULL AUTO_INCREMENT,
 	name CHAR(32) NOT NULL,
 	color CHAR(32) NOT NULL,
   price CHAR(32) NOT NULL,
   inventory CHAR(32) NOT NULL,
   melts BOOLEAN NOT NULL,
-  created_at timestamp DEFAULT NULL
-  updated_at timestamp DEFAULT NULL
-  deleted_at timestamp DEFAULT NULL
+  created_at timestamp NULL DEFAULT NULL,
+  updated_at timestamp NULL DEFAULT NULL,
+  deleted_at timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (id)
 );
