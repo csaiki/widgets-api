ee509fdc40ec7b796f1ad700e5f0fc097f47db6b
christiansaiki <christian@revmob.com>
2017-08-11 11:00:40 -0300

    changed tcp connection ip

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM golang:1.8.3-alpine3.6
 
 RUN apk update && apk upgrade && \
-    apk add --no-cache bash git openssh && \
+    apk add --no-cache bash git openssh mysql-client && \
     mkdir -p /app
 
 WORKDIR /go/src/app
--- src/db.go
+++ src/db.go
@@ -5,7 +5,7 @@ import "github.com/jinzhu/gorm"
 // InitDb connects to a MySQL database
 func InitDb() *gorm.DB {
 	//open a db connection
-	db, err := gorm.Open("mysql", "chris:1234@/widgetsapi?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", "chris:1234@tcp(db:3306)/widgetsapi?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic("failed to connect database")
 	}
