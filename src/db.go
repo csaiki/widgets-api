63b036580cc4e0d8e23cdc3f1ef1114ff0215697
christiansaiki <christian@revmob.com>
2017-08-11 15:24:09 -0300

    added env variable DOCKER to connect to database

--- src/db.go
+++ src/db.go
@@ -1,11 +1,24 @@
 package src
 
-import "github.com/jinzhu/gorm"
+import (
+	"fmt"
+	"os"
+
+	"github.com/jinzhu/gorm"
+)
 
 // InitDb connects to a MySQL database
 func InitDb() *gorm.DB {
+	var host string
+	if os.Getenv("DOCKER") == "true" {
+		host = "tcp(db:3306)"
+	} else {
+		host = ""
+	}
+	connString := fmt.Sprintf("%s%s%s", "chris:1234@", host, "/widgetsapi?charset=utf8&parseTime=True&loc=Local")
+	fmt.Println(connString)
 	//open a db connection
-	db, err := gorm.Open("mysql", "chris:1234@tcp(db:3306)/widgetsapi?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", connString)
 	if err != nil {
 		panic("failed to connect database")
 	}
